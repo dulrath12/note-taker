222028e676bcfe1070d0ca11d4732bbbfa38631d
Dakotah Ulrath <dulrath@live.com>
2022-06-19 20:56:15 -0400

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const notes = require('./db/db')
+const { notes } = require('./db/db')
 
 const PORT = process.env.PORT || 3001
 const app = express()
